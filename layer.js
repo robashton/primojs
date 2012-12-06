be150a3e3a29ae2cc9ccbda720f06f1df1bd5e50
Rob Ashton <robashton@codeofrob.com>
2012-12-06 13:23:27 +0100

    Dragging and painting

--- layer.js
+++ layer.js
@@ -12,9 +12,12 @@ define(function(require) {
       for(var x = 0; x < this.config.width; x++) {
         for(var y = 0; y < this.config.height; y++) {
           var index = x + (y * this.config.width)
-          var left = x * this.tileset.tilesize
-          var top = y * this.tileset.tilesize
-          this.image.drawTo(context, this.config.data[index], left, top);
+          var left = x * this.config.tilesize
+          var top = y * this.config.tilesize
+          this.image.drawTo(context, 
+            this.config.data[index], 
+            left, 
+            top); // TODO: Specify width 
         }
       }
     }
