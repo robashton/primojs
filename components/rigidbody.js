453fcc15910e1493499ba080bcd0df941c21cba9
Rob Ashton <robashton@codeofrob.com>
2013-01-08 10:14:28 +0000

    Added the notion of group to the rigid body

--- components/rigidbody.js
+++ components/rigidbody.js
@@ -1,11 +1,14 @@
 define(function(require) {
   var _ = require('underscore')
+  var util = require('../commons')
 
-  var RigidBody = function(entity) {
+  var RigidBody = function(entity, options) {
+    options = options || {}
     this.entity = entity
     this.entity.on('collided', _.bind(this.onCollided, this))
     this.entity.handle('collidewith', _.bind(this.collideWith, this))
     this.entity.collideable = true
+    this.group = util.valueOrDefault(options.group, 'none')
   }
 
   RigidBody.prototype = {
@@ -15,6 +18,9 @@ define(function(require) {
     collideWith: function(other) {
       var entityOne = this.entity
         , entityTwo = other.entity
+
+      if(this.group === other.group && this.group !== 'none')
+        return
 
       entityOne.velx = 0
       entityOne.vely = 0
