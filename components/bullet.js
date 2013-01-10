e38030005d003fb51157ab8fa1594d8f5271cee1
Rob Ashton <robashton@codeofrob.com>
2013-01-10 20:01:02 +0000

    Bullets are physical /core

--- components/bullet.js
+++ components/bullet.js
@@ -3,6 +3,7 @@ define(function(require) {
   var Bullet = function(entity) {
     this.entity = entity
     this.entity.on('collision', this.onCollision, this)
+    this.entity.physical = true
   }
 
   Bullet.prototype = {
