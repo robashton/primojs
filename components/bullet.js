220ef1fab7607719cb0adbcaf7d6f51b9922074d
Rob Ashton <robashton@codeofrob.com>
2013-01-10 20:44:36 +0000

    bullet is colldeable, not physical /core

--- components/bullet.js
+++ components/bullet.js
@@ -2,12 +2,12 @@ define(function(require) {
 
   var Bullet = function(entity) {
     this.entity = entity
-    this.entity.on('collision', this.onCollision, this)
-    this.entity.physical = true
+    this.entity.on('collided', this.onCollided, this)
+    this.entity.collideable = true
   }
 
   Bullet.prototype = {
-    onCollision: function(other) {
+    onCollided: function(other) {
       other.dispatch('hitbybullet')
       this.entity.kill()
     }
